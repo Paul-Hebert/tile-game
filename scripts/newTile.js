ea63b5f6980924ea79cca24c52df2b006535389f
Paul Hebert <=>
2022-05-21 20:03:01 -0700

    Stub out roads

--- scripts/newTile.js
+++ scripts/newTile.js
@@ -1,8 +1,35 @@
+import { randomBool } from "./random.js";
+
 export const newTile = ({ gridEl }) => {
   const tileEl = document.createElement("div");
+  const roadSides = {
+    top: randomBool() ? "road" : "grass",
+    left: randomBool() ? "road" : "grass",
+    bottom: randomBool() ? "road" : "grass",
+    right: randomBool() ? "road" : "grass",
+  };
+
+  let svgContent = "";
+  if (roadSides.top === "road") {
+    svgContent += addLine(50, 0);
+  }
+  if (roadSides.left === "road") {
+    svgContent += addLine(0, 50);
+  }
+  if (roadSides.right === "road") {
+    svgContent += addLine(100, 50);
+  }
+  if (roadSides.bottom === "road") {
+    svgContent += addLine(50, 100);
+  }
+
   tileEl.classList.add("tile-wrapper");
   tileEl.innerHTML = /* html */ `
-    <div class="tile-handle"><div class="tile-content"></div></div>
+    <div class="tile-handle">
+      <svg viewBox="0 0 100 100" class="tile-content">
+        ${svgContent};
+      </svg>
+    </div>
   `;
   gridEl.append(tileEl);
   return {
@@ -15,3 +42,9 @@ export const newTile = ({ gridEl }) => {
     },
   };
 };
+
+function addLine(x, y) {
+  return /* svg */ `
+      <line stroke="#000" x1="50" y1="50" x2="${x}" y2="${y}"></line>
+    `;
+}
---
+++ scripts/random.js
@@ -0,0 +1,28 @@
+function random() {
+  const isArray = Array.isArray(arguments[0]);
+
+  if (isArray) {
+    const targetArray = arguments[0];
+
+    return targetArray[random(0, targetArray.length - 1, true)];
+  } else {
+    const min = arguments[0];
+    const max = arguments[1];
+    const clamp = arguments[2] || false;
+
+    const val = Math.random() * (max - min) + min;
+
+    return clamp ? Math.round(val) : val;
+  }
+}
+
+function randomBool(chance = 0.5) {
+  return Boolean(Math.random() <= chance);
+}
+
+// https://stackoverflow.com/a/4550514/7816145
+function randomItemInArray(array) {
+  return array[Math.floor(Math.random() * array.length)];
+}
+
+export { random, randomBool, randomInt, randomItemInArray };
